b3ab64dc3d11d55c60d6ecea8fe1d2685f78d9b9
Andrea Stagi <stagi.andrea@gmail.com>
2019-09-10 14:56:38 +0200

    Down containers

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ docker image prune -f
 if [ "$1" = "release" ]; then
     docker-compose -f docker-compose.prod.yml build
     docker-compose -f docker-compose.prod.yml stop
+    docker-compose -f docker-compose.prod.yml down
     docker-compose -f docker-compose.prod.yml up -d
 fi
