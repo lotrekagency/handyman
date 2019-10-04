78bf89e08c9612535648bedd62817042cdd5e7c3
System Administrator <root@MacBook-Pro-di-Marco.local>
2019-10-04 17:49:53 +0200

    added some useful data on machine model

--- main/admin.py
+++ main/admin.py
@@ -48,8 +48,10 @@ class DeadlineInline(admin.TabularInline):
 
 class MachineAdmin(admin.ModelAdmin):
     fieldsets = (
-        (_('General'), {'fields': ('name', 'reseller', 'end_time')}),
+        (_('General'), {'fields': ('name', 'reseller', 'end_time','root_permissions','management_contract','price','price_period')}),
+        (_('Online Panel'), {'fields': ('administration_panel', 'administration_panel_username', 'administration_panel_password')}),
         (_('Ssh'), {'fields': ('server_address', 'ssh_username', 'ssh_password', 'notes')}),
+        
     )
     list_display = ('name', 'server_address', 'reseller', 'end_time')
     search_fields = ('name', 'server_address')
